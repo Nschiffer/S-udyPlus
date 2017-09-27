47bdf3f39af30a3e736a38bccdf2118ec316f3d9
Naomi Schiffer <nschiffer@aapl000612.local>
2017-09-27 09:15:42 -0400

    we went four steps back, but we got a button to print. But we deleted the button. YAAAAY!

--- S+udyPlus/ViewController.swift
+++ S+udyPlus/ViewController.swift
@@ -13,13 +13,22 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
+    
+
+    @IBAction func button1(_ sender: Any) {
+    print("hello")
+    }
+    
+
+}
+
+
+
+
+
+
+
+
 
