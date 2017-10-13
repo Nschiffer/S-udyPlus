372dc8a7645c3f9cf2ba742f581239af022a62d1
Naomi Schiffer <nschiffer@aapl000612.local>
2017-10-13 08:43:39 -0400

    we are going through todos. have 1 and 2 done

--- S+udyPlus/ViewController.swift
+++ S+udyPlus/ViewController.swift
@@ -13,12 +13,12 @@ class ViewController: UIViewController {
     var questions = ["Which layer is made of the Earth is made of solid iron and nickel?","What layer of the earth is liquid?", "What are the three types of rocks?", "What type of rock is made from sediments?", "What type of rock is made from magma?", "What is a light colored igneous rock that is found in continental crust?", "What is the lithosphere?", "What is the asthenosphere?", "What is Pangaea?", "Who thought of the continental drift theory?", "What is a seismic wave?" ]
     
     
-    var answers = [["Mantle", "Inner core", "outer core", "crust"],["Mantle", "outer core", "inner core", "crust"],["metamorphic, igneous, sedimentary", "limestone, granite, basalt","quartz, stone, yellowstone", "solid, liquid, gas"],["Metamorphic rock","Igneous rock","Asteroids","Sedimentary rock"],["limestone", "Pangaea","igneous rock", "schist"],["granite","obsidian", "breccia","gneiss"],["the sphere that surrounds earth and protect all living things", "a species of endangered animals", "the uppermost part of the mantle and crust","a shape"],["the part of the earth that the lithosphere floats on","a shape", "the shere around the earth that protects all living organisms","a shoe brand"],["a famous rock band","the landmass of all the continents connected 200 million years ago","an endangered animal species", "a newly discovered planet"], ["Esther Bunnie","Alfred Deerfield","Mary Wedge","Alfred Wegener"],["a","b","c","d"]]
+    var answers = [["Mantle", "Inner core", "outer core", "crust"],["Mantle", "outer core", "inner core", "crust"],["metamorphic, igneous, sedimentary", "limestone, granite, basalt","quartz, stone, yellowstone", "solid, liquid, gas"],["Metamorphic rock","Igneous rock","Asteroids","Sedimentary rock"],["limestone", "Pangaea","igneous rock", "schist"],["granite","obsidian", "breccia","gneiss"],["the sphere that surrounds earth and protect all living things", "a species of endangered animals", "the uppermost part of the mantle and crust","a shape"],["the part of the earth that the lithosphere floats on","a shape", "the shere around the earth that protects all living organisms","a shoe brand"],["a famous rock band","the landmass of all the continents connected 200 million years ago","an endangered animal species", "a newly discovered planet"], ["Esther Bunnie","Alfred Deerfield","Mary Wedge","Alfred Wegener"],["the effect from a volcano","the effect from an earthquake","a radioactive wave","a huge ocean wave"]]
     
-    //This is a variable that represents the quetsion number
-    var questionNumber = 0
+   
+    var questionNumber = 3
     
-    //TODO #2 - Create an array with the index of all the correct answers
+   var correctAnswer = [1,1,0,3,2,0,2,0,1,3,1]
 
     
 
@@ -48,11 +48,11 @@ class ViewController: UIViewController {
     }
     
     @IBAction func button3(_ sender: Any) {
-        //TODO - add the checkAnswer function
+        checkAnswer(buttonNumber: 2)
     }
     
     @IBAction func button4(_ sender: Any) {
-        //TODO - add the checkAnswer function
+        checkAnswer(buttonNumber: 3)
     }
  
     
@@ -86,6 +86,9 @@ class ViewController: UIViewController {
     func displayQuestion() {
         question1.text = questions[questionNumber]
         button1point5.setTitle(answers[questionNumber][0], for: .normal)
+        buton2point5.setTitle(answers[questionNumber][1], for: .normal)
+        button3point5.setTitle(answers[questionNumber][2], for: .normal)
+        button4point5.setTitle(answers[questionNumber][3], for: .normal)
         /*TODO #1
          Finish setting the titles for the other 3 answer buttons. When you have done this, play with the questionNumber variable to make sure it displays all of the questions.
         */
